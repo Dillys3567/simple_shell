ec3f67fd824b344afebfb82b66a913fe242cc03c
Posei3011 <posei3011@gmail.com>
2023-06-19 03:31:02 -0700

    Update

--- shell.h
+++ shell.h
@@ -1,6 +1,18 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <errno.h> 
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h> 
+
 
 
 #endif /* SHELL_H */
