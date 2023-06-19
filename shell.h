702d44d4b2cdcf4062990d014a98e6f5fa3e143e
Posei3011 <posei3011@gmail.com>
2023-06-19 03:57:21 -0700

    shell.c update

--- shell.h
+++ shell.h
@@ -13,6 +13,22 @@
 #include <signal.h>
 #include <fcntl.h> 
 
+             /*To add, remove or show aliases*/
+int output_alias(data_of_programs *data, char *alias);
 
+                  /*To get the alias*/
+char *fetch_alias(data_of_programs *data, char *name);
+
+                  /*To add alias*/
+int write_alias(char *string_alias, data_of_programs *data);
+
+       /*To allow to read a line from command prompt*/
+int _getlines(data_of programs * data);
+
+   /*To check for logical operators and split*/
+int check_ops_logic(char *array_of_commands[], int x,
+		char array_of_operators[]);
+
+    /*
 
 #endif /* SHELL_H */
