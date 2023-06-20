477b38f5880a671d3d84698bedd70e01318343e4
Posei3011 <posei3011@gmail.com>
2023-06-20 02:27:41 -0700

    Update

--- shell.h
+++ shell.h
@@ -29,6 +29,33 @@ int _getlines(data_of programs * data);
 int check_ops_logic(char *array_of_commands[], int x,
 		char array_of_operators[]);
 
-    /*
+    /*exit program with status*/
+int exit_builtin(data_of_programs *data);
+
+   /*change working dir*/
+int cd_builtin(data_of_programs *data);
+
+      /*set working dir*/
+int set_working_directory(data_of_programs *data, char *new_dir);
+
+      /*show environment of shell*/
+int help_builtin(data_of_programs *data);
+
+       /*aliases*/
+int alias_builtin(data_of_program *data);
+
+    /*search and execute builtins match*/
+int list_of_builtins(data_of_programs *data);
+
+    /*show shell environment*/
+int builtin_environment(data_of_programs *data);
+
+        /*set env*/
+int builtin_set_environment(data_of_programs *data);
+
+       /*unset env*/
+int builtin_unset_environment(data_of_program *data);
+
+      /*
 
 #endif /* SHELL_H */
