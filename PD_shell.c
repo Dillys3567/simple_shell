757be27f4cf7c0f61aa0d494983d11674a098849
Posei3011 <posei3011@gmail.com>
2023-06-20 05:48:25 -0700

    Update

---
+++ PD_shell.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+ * main - initialize the variables of the program
+ * @argc: number of values gotten from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: Nothing (zero on succes).
+ */
+int mai(int argc, char *argv[], char *env[])
+{
+	
